5543edf8bb228cda0f3b44757705fa9ee3d8e88a
Raad-Hanandeh <138323419+Raad-Hanandeh@users.noreply.github.com>
2024-03-01 21:22:50 +0300

    last

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,14 @@ module.exports = {
       '#000': '#000000',
       '#9f9f9f':'9f9f9f',
       '#333333':'#333333',
-      '#3a3a3a':'#3a3a3a'
+      '#3a3a3a':'#3a3a3a',
+      'regal-blue': '#243c5a',
+      '#816dfa':'#816dfa'
       },
       maxWidth: {
         'disktop': '76.97rem',
+        'disktopp2': '77.25rem',
+        'text':'1026px'
         
         
       },
@@ -27,24 +31,77 @@ module.exports = {
         '32px':'32px',
         '1400px':'1400px',
         '443px':'443px',
-        '74px':'74px'
+        '74px':'74px',
+        '316px':'316px',
+        '60px':'60px',
+        '270px':'270px',
+        '301px':'301px',
+        '100px':'100px',
+        '500px':'500px',
+        '30PX':'30PX',
+        '64px':'64px',
+        '77px':'77px',
        
       },
       width: {
+       
         '404px':'404px',
         '372px':'372px',
         '50px':'50px',
         '643px':'643px',
-        '222px':'222px'
+        '222px':'222px',
+        '60px':'60px',
+        '98px':'98px',
+        '53px':'53px',
+        '285px':'285px',
+        '423px':'423px',
+        '18px':'18px',
+        '30PX':'30PX',
+        '215px':'215px',
+        '123px':'123px',
         
+        
+      },
+      spacing: {
+        '77px': '77px',
       },
       fontSize: {
         "40px": '40px',
         '34px':'34px',
         '52px':'52px',
-        '18px':'18px'
+        '18px':'18px',
+        '25px':'25px',
+        "42px": '42px',
+        '24px':'24px',
+        '13px':'13px',
+        '16px':'16px',
+        '14px':'14px'
 
-      }
+      },
+      margin: {
+        '121px': '121px',
+        '70px': '70px',
+        '85px': '85px',
+        '13px': '13px',
+        '38px':'38px',
+        '20px':'20px',
+        '30px':'30px',
+        '14px':'14px',
+        '16px':'16px',
+        '41px':'41px',
+        '18px':'18px',
+        '53px':'53px'
+      
+      },
+      padding: {
+        '100px': '100px',
+        '53px': '53px',
+        '38px':'38px'
+        
+      },
+      fontFamily: {
+        'Poppins': ['"Poppins"',...defaultTheme.fontFamily.Poppins],
+      },
     },
   },
   plugins: [],
